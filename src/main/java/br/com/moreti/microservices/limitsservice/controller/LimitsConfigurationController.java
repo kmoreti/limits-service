b04be15928c9b249f600b1fbde977fc2ed4a5329
kmoreti <kleber.camargo@fatec.sp.gov.br>
2018-03-15 22:01:29 -0300

    Added hystrix.

--- src/main/java/br/com/moreti/microservices/limitsservice/controller/LimitsConfigurationController.java
+++ src/main/java/br/com/moreti/microservices/limitsservice/controller/LimitsConfigurationController.java
@@ -2,6 +2,7 @@ package br.com.moreti.microservices.limitsservice.controller;
 
 import br.com.moreti.microservices.limitsservice.config.Configuration;
 import br.com.moreti.microservices.limitsservice.model.LimitConfiguration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +18,15 @@ public class LimitsConfigurationController {
     @GetMapping("/limits")
     public LimitConfiguration retrieveLimitsFromConfigurations() {
         return new LimitConfiguration(configuration.getMaximum(),configuration.getMinimum());
+    }
+
+    @GetMapping("/fault-tolerance-example")
+    @HystrixCommand(fallbackMethod = "fallbackRetrieveConfiguration")
+    public LimitConfiguration retrieveConfiguration() {
+        throw new RuntimeException("Not available!");
+    }
+
+    public LimitConfiguration fallbackRetrieveConfiguration() {
+        return new LimitConfiguration(999,9);
     }
 }
